c4c5e176f5689770a785a0378785e5a3d12b597a
WonDuJin <windyjan1@naver.com>
2022-08-23 18:29:26 +0900

    js

---
+++ js/08.17/practics-1.js
@@ -0,0 +1,18 @@
+var sangho = {
+  name : "이상호",
+  age : 85
+}
+
+var doojin = {
+  name : "원두진",
+  age : 135
+}
+
+if(sangho.age > doojin.age || typeof(sangho.name)==="string"){
+
+  console.log(sangho.name + "님이 형입니다.");
+
+}else {
+  console.log(doojin.name + "님이 형입니다.");
+}
+
---
+++ js/08.18/studentList.js
@@ -0,0 +1,30 @@
+let studentList = [
+  "강예훈","김성현","류주완","마근원","문예찬","박종인","박재형","송형주","양상희","원두진","유민호","이상호","이소영","이은수","정동욱","정윤환","정정원","지영빈","한용준","황초영","김근수","김승현","김지애","전형민","정연주","이아연","최화연","한가람"
+];
+
+let count = 0;
+for( let i = 0; i < studentList.length; i++){ 
+  
+  if(count[i]=studentList[i]){
+    
+    count++;    
+  } 
+
+}
+console.log(count);
+
+
+let name = "공욱재";
+
+studentList.splice(28,0,"공욱재");
+console.log(studentList);
+console.log(studentList.length);
+
+for (let i = 0; i < studentList.length; i++){
+
+  console.log(studentList[i] + "님");
+
+}
+
+
+
