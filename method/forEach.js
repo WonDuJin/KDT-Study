32b8fb28ed5dea32e1edcfdfd596f66f6d490e63
WonDuJin <windyjan1@naver.com>
2022-08-24 17:48:48 +0900

    forEach메서드연습

---
+++ method/forEach.js
@@ -0,0 +1,51 @@
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+/*const fruit = arr.push("딸기","망고");
+
+console.log(fruit);
+
+console.log(arr);
+
+const number = arr.pop()
+
+console.log(number);
+
+console.log(arr);
+
+console.log(arr.shift());
+
+console.log(arr);
+
+console.log(arr.unshift("바나나"));
+
+console.log(arr);*/
+
+
+console.log(arr.indexOf(10,2));
+
+
+/*function dojinLoop(arr){
+  //일반 명령방식
+  for(let i = 0; i < arr.length; i++){
+    if(Array.isArray(arr)){
+      console.log(arr[i]);
+    }
+  }
+}
+
+
+for(let i = 0; i < arr.length; i++){
+  if(Array.isArray(arr)){
+    console.log(arr[i]);
+  }
+}
+
+
+
+//배열 메서드 자바스크립트, 익명함수 방식
+arr.forEach(function(a, b){
+console.log(a+ "값은 인덱스 : " + b);
+})
+
+//화살표 함수 축약방식
+arr.forEach((a)=>console.log(a));*/
