8663d768b829741a6e815298ec02c6a66629140a
WonDuJin <windyjan1@naver.com>
2022-08-24 17:13:14 +0900

    push연습

--- js/08.18/studentList.js
+++ js/08.18/studentList.js
@@ -2,7 +2,12 @@ let studentList = [
   "강예훈","김성현","류주완","마근원","문예찬","박종인","박재형","송형주","양상희","원두진","유민호","이상호","이소영","이은수","정동욱","정윤환","정정원","지영빈","한용준","황초영","김근수","김승현","김지애","전형민","정연주","이아연","최화연","한가람"
 ];
 
-let count = 0;
+const name = studentList.push("공욱재");
+
+console.log(studentList);
+
+
+/*let count = 0;
 for( let i = 0; i < studentList.length; i++){ 
   
   if(count[i]=studentList[i]){
@@ -24,7 +29,7 @@ for (let i = 0; i < studentList.length; i++){
 
   console.log(studentList[i] + "님");
 
-}
+}*/
 
 
 
