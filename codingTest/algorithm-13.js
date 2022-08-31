512e69e965be5cd436ae32971d01180735689cfc
WonDuJin <windyjan1@naver.com>
2022-08-31 22:58:03 +0900

    Advanced B까지 작업성공

--- codingTest/algorithm-13.js
+++ codingTest/algorithm-13.js
@@ -2,18 +2,18 @@
 
 /*console.log(number);*/
 
-const randomList=[];
-for(let i = 1; i < 3; i++){
+const inputData=[];
+for(let i = 1; i <= 3; i+=1){
   let random = Math.floor(Math.random()*20)+1;
-  if (randomList.indexOf(random)=== -1){
-    randomList.push(random);
+  if (inputData.indexOf(random)=== -1){
+    inputData.push(random);
   } else{
     i--
   }
 }
-console.log(randomList);
+console.log(inputData);
 
-const average = randomList.reduce((a,c)=>a+c)/randomList.length;
+const average = inputData.reduce((a,c)=>a+c)/inputData.length;
 console.log(average);
 
 if(average >=10){
@@ -21,3 +21,12 @@ if(average >=10){
 }if(average < 10){
   console.log("under");
 }
+const objectData = { first : 0, second : 0, third: 0};
+function insert (object, inputData){
+  object["first"] = inputData[0];
+  object["second"] = inputData[1];
+  object["third"] = inputData[2];
+}
+insert(objectData,inputData);
+
+console.log(objectData);
